df461e39e8d0c95144cf27ef46b9bcda99d6a4e8
f3r21 <31838677+f3r21@users.noreply.github.com>
2026-05-20 00:42:27 -0500

    update margherita cask v0.1.0 to point to f3r21/Margherita repository and remove emojis

--- Casks/margherita.rb
+++ Casks/margherita.rb
@@ -2,10 +2,10 @@ cask "margherita" do
   version "0.1.0"
   sha256 :no_check
 
-  url "https://github.com/f3r21/claude-usage-menubar/releases/download/v#{version}/Margherita.dmg"
+  url "https://github.com/f3r21/Margherita/releases/download/v#{version}/Margherita.dmg"
   name "Margherita"
   desc "Native macOS menu bar Claude Code rate limits indicator"
-  homepage "https://github.com/f3r21/claude-usage-menubar"
+  homepage "https://github.com/f3r21/Margherita"
 
   app "Margherita.app"
 
