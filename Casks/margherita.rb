3ea8d0e294372a0f459ae1fde74ea94ad13e69a6
f3r21 <31838677+f3r21@users.noreply.github.com>
2026-06-01 10:23:49 -0500

    margherita 0.2.0: pin sha256 to published DMG, add re-sign caveats

--- Casks/margherita.rb
+++ Casks/margherita.rb
@@ -1,6 +1,6 @@
 cask "margherita" do
-  version "0.1.0"
-  sha256 :no_check
+  version "0.2.0"
+  sha256 "79f3b5f629d4e5fb1106aa49ea6ec92e1ff3d08fd60fd7680473de439e8bb8bd"
 
   url "https://github.com/f3r21/Margherita/releases/download/v#{version}/Margherita.dmg"
   name "Margherita"
@@ -8,6 +8,12 @@ cask "margherita" do
   homepage "https://github.com/f3r21/Margherita"
 
   app "Margherita.app"
+
+  caveats <<~EOS
+    Margherita is ad-hoc signed. After installing, run this once to fix the
+    "damaged / unidentified developer" launch error:
+      codesign --force --deep --sign - /Applications/Margherita.app && xattr -d com.apple.quarantine /Applications/Margherita.app
+  EOS
 
   zap trash: [
     "~/Library/Preferences/local.margherita.plist",
